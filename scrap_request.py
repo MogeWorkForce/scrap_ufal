cd5a63d599a511d9d4127c35a81925410e41fa8d
hermogenes <exemogenes@hotmail.com>
2015-12-12 23:15:27 -0300

    capturando links nos documentos relacionados da nota de empenho

--- scrap_request.py
+++ scrap_request.py
@@ -9,7 +9,14 @@ match = re.compile(r'<table class="tabela">(.*?)<\/table>')
 get_paginator = re.compile(r'<span[^>]*class="paginaAoRedor">(.*?)</span>')
 links = re.compile(r'<a[^>]*href="(?P<links>.*)?">.*?</a>')
 match_th = re.compile(r'<th[^>]*?>(?P<content_th>[^<]*?)</th>')
-match_td = re.compile(r'<td[^>]*?>(?P<content_td>[^<]*?)</td>')
+match_td = re.compile(r'<(?:td|a)[^>]*?(?:href="(?P<link_document>[^"]*?)")?>(?P<content_td>[^<]*?)</(?:td|a)>')
+match_tr_content = re.compile(
+        r'<(?:td|span|tr)\s?(?:colspan="[^"]*?"|class="(?P<class>[^"]*?)")?>(?P<content_line>[^<]*?)<\/(?:td|span|tr)>'
+    )
+
+match_tr = re.compile(
+        r'<tr.*?>(?P<content_tr>.*?)<\/tr>'
+    )
 
 def get_content_page(url, original_link=None):
     result = requests.get(url)
@@ -19,22 +26,22 @@ def get_content_page(url, original_link=None):
 def load_content(content):
     table_content = match.findall(content)
     for table in table_content:
+        #for i, content in enumerate(match_tr.finditer(table)):
+        #    print i, content.group('content_tr')
         for th in match_th.finditer(table):
             print th.group("content_th")
 
         for td in match_td.finditer(table):
-            print td.group("content_td")
+            print td.group("content_td"), td.group('link_document')
 
 def clean_result(result):
     return result.text.replace('\n', '').replace('  ', '')
 
-#url = 'http://www.portaltransparencia.gov.br/despesasdiarias/empenho?documento=153037152222015NE800115http://www.portaltransparencia.gov.br/despesasdiarias/empenho?documento=153037152222015NE800115'
 url = 'http://www.portaltransparencia.gov.br/despesasdiarias/empenho?documento=153037152222015NE800115'
 url_base = "http://www.portaltransparencia.gov.br"
 
 result = requests.get(url)
 no_spaces = clean_result(result)
-
 print url
 load_content(no_spaces)
 
@@ -45,7 +52,7 @@ for pg in paginator:
     for link_url in links.finditer(pg):
         link_ = link_url.group('links')
         print link_
-        time.sleep(5)
+        time.sleep(1)
         if link_ not in visited_link:
             get_content_page(url_base+link_)
             visited_link.append(link_)
