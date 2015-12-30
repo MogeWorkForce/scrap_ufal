afe00d2c8a04946b6dcecd83107789e756d1dd38
hermogenes <exemogenes@hotmail.com>
2015-12-29 21:03:16 -0300

    change access content and context of variable type

--- helper.py
+++ helper.py
@@ -4,13 +4,17 @@ from __future__ import absolute_import, unicode_literals
 class Reader(object):
     def __init__(self, arq, *args, **kwargs):
         self.name_arq = arq
-        self._content = []
+        self.__content = ''
     
     @property
     def text(self):
-        if self._content:
-            return self._content
+        if self.__content:
+            return self.__content
         else:
             self.temp = open(self.name_arq, 'rb')
-            self._content = self.temp.read().decode('utf-8')
-            return self._content
+            self.__content = self.temp.read().decode('utf-8')
+            return self.__content
+
+    @property
+    def content(self):
+        return self.text
