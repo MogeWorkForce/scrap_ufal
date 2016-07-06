0596c48744c6b4eee0725e5a8a278b43d8b0ad10
HermogenesBatista <exemogenes@hotmail.com>
2016-07-05 23:12:26 -0300

    changed run Product

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-python main.py -u "http://portaltransparencia.gov.br/despesasdiarias/empenho?documento=153037152222015NE800115" -b 30 -i > scrap.log 2>&1&
+gunicorn -w 2 -b 0.0.0.0:8080 entry_point:app > scrap.log 2>&1&
 
-gunicorn -w 3 -b 0.0.0.0:8080 entry_point:app
+python main.py -u "http://portaltransparencia.gov.br/despesasdiarias/empenho?documento=153037152222015NE800115" -b 10 -i -j 2
