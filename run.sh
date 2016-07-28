57565547dcb8de6dbd44f3d328123584f0d237de
HermogenesBatista <exemogenes@hotmail.com>
2016-07-27 21:41:17 -0300

    run entry_point first

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
+gunicorn -w 2 -b 0.0.0.0:8080 scrap_portal.api.entry_point:app &
+
 if [ "$MIGRATE_DOCUMENTS" = true ]; then
     echo "Starting adapt documents!"
     python -m scrap_portal.utils.adapt_docs
 fi
-
-gunicorn -w 2 -b 0.0.0.0:8080 scrap_portal.api.entry_point:app &
 
 python -m scrap_portal.main -u "http://portaltransparencia.gov.br/despesasdiarias/empenho?documento=153037152222015NE800115" -b $BATCH_URLS -i
